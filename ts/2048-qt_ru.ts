bfbf9dcf9d955dd6df982a4818459352974614eb
Sergey Basalaev <sbasalaev@gmail.com>
2014-05-17 13:30:44 +0700

    Russian translation

---
+++ ts/2048-qt_ru.ts
@@ -0,0 +1,138 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>main</name>
+    <message>
+        <location filename="../qml/main.qml" line="13"/>
+        <source>2048 Game</source>
+        <translation>Игра 2048</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="24"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="26"/>
+        <location filename="../qml/main.qml" line="264"/>
+        <source>New Game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="31"/>
+        <source>Exit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="38"/>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="40"/>
+        <source>Labeling</source>
+        <translation>Метки</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="42"/>
+        <source>2048</source>
+        <translation>2048</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="54"/>
+        <source>Degree</source>
+        <translation>Степень</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="66"/>
+        <source>Military Rank</source>
+        <translation>Военный ранг</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="78"/>
+        <source>PRC</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="91"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="93"/>
+        <source>English</source>
+        <translation>Английский</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="105"/>
+        <source>Simplified Chinese</source>
+        <translation>Упрощённый китайский</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="121"/>
+        <source>Help</source>
+        <translation>Справка</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="123"/>
+        <source>About</source>
+        <translation>О программе</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="127"/>
+        <source>About Qt</source>
+        <translation>О Qt</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="185"/>
+        <source>SCORE</source>
+        <translation>СЧЁТ</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="185"/>
+        <source>BEST</source>
+        <translation>ЛУЧШИЙ</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="245"/>
+        <source>Join the numbers and get to the &lt;b&gt;2048 tile&lt;/b&gt;!</source>
+        <translation>Объединяйте числа, чтобы получить &lt;b&gt;плитку 2048&lt;/b&gt;!</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="302"/>
+        <source>Language Setting Hint</source>
+        <translation>Подсказка установки языка</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="303"/>
+        <source>Please restart the program to make the language setting take effect.</source>
+        <translation>Пожалуйста, перезапустите программу, чтобы применить языковые настройки.</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="309"/>
+        <source>About 2048-Qt</source>
+        <translation>О программе 2048-Qt</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="316"/>
+        <source>Game Over</source>
+        <translation>Игра окончена</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="317"/>
+        <source>Game Over!</source>
+        <translation>Игра окончена!</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="327"/>
+        <source>You Win</source>
+        <translation>Вы выиграли</translation>
+    </message>
+    <message>
+        <location filename="../qml/main.qml" line="328"/>
+        <source>You win! Continue playing?</source>
+        <translation>Вы выиграли! Продолжить играть?</translation>
+    </message>
+</context>
+</TS>
